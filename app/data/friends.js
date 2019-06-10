34152634af3560586085421a172c78d9b5bb8ab1
Stephanie Sapp <stephs1531@gmail.com>
2019-06-10 08:41:42 -0500

    set up friends data file

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+import { builtinModules } from "module";
+
+//============================================================
+//DATA
+//store survey results in an array that can be called by other pages
+//============================================================
+
+var friendsArray = [
+    {
+        "name":"Steph",
+        "photo":"https://scontent.ford4-1.fna.fbcdn.net/v/t1.0-9/24796317_10214971036499793_6501910851104096089_n.jpg?_nc_cat=102&_nc_oc=AQm72u2NtUV6B_9kk8y2LbWQl7FxhHGgq8FyQ116F64DCDxmmz7QLqmXW4tchM6PPU0&_nc_ht=scontent.ford4-1.fna&oh=a745f4a07661f413e077d6967184998d&oe=5D92D00B",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+];
+
+modules.exports = friendsArray;
