5c882f9802ff27734004dbeef4087b205b635f06
Stephanie Sapp <stephs1531@gmail.com>
2019-06-11 08:42:04 -0500

    got routes working

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -9,6 +9,10 @@ var path = require("path");
 
 module.exports = function(app) {
 
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
     app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
